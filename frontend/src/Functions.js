3bd88d90dfdee7666249757f70f99538986803dd
Abishek khadka <132669162+Abishek-khadka01@users.noreply.github.com>
2024-11-20 19:12:19 +0545

    Update

--- frontend/src/Functions.js
+++ frontend/src/Functions.js
@@ -1,8 +1,8 @@
 const GOOGLE_CLIENT_ID =
-  "888376131566-oc8698u9ahh1oa40ks514t40m3gvc86k.apps.googleusercontent.com";
-const GOOGLE_CLIENT_SECRET = "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";
-const GOOGLE_REDIRECT_URL = "http://localhost:4000/auth/google";
-const GOOGLE_ENDPOINT_URL = "http://localhost:4000";
+  "";
+const GOOGLE_CLIENT_SECRET = "";
+const GOOGLE_REDIRECT_URL = "";
+const GOOGLE_ENDPOINT_URL = ""
 
 export function getGooglgeOauthUrl() {
   const roolUrl = "https://accounts.google.com/o/oauth2/v2/auth";
